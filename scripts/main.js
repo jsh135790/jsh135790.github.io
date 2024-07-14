ec1a73ce32150331c3aba9859fb03c5c950f1999
jsh135790 <1072519402@qq.com>
2024-07-14 22:48:59 +0800

    updated 7.14

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-let myImage = document.querySelector("img");
+// let myImage = document.querySelector("img");
 
 // document.querySelector("img").addEventListener("click", () => {
 //     let mySrc = myImage.getAttribute("src");
@@ -9,35 +9,35 @@ let myImage = document.querySelector("img");
 //     }
 // });
   
-myImage.onclick = function () {
-  let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/cisco-png-logo.png") {
-    myImage.setAttribute("src", "images/cisco2.png");
-  } else {
-    myImage.setAttribute("src", "images/cisco-png-logo.png");
-  }
-};
+// myImage.onclick = function () {
+//   let mySrc = myImage.getAttribute("src");
+//   if (mySrc === "images/cisco-png-logo.png") {
+//     myImage.setAttribute("src", "images/cisco2.png");
+//   } else {
+//     myImage.setAttribute("src", "images/cisco-png-logo.png");
+//   }
+// };
 
 // document.querySelector("html").addEventListener("click", function () {
 //     alert("别戳我，我怕疼。");
 // });
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-function setUserName() {
-    let myName = prompt("请输入你的名字。");
-    if(!myName){
-        setUserName();
-    }else{
-        localStorage.setItem("name", myName);
-        myHeading.textContent = "Mozilla 酷毙了，" + myName;
-    }
-}
-if (!localStorage.getItem("name")) {
-    setUserName();
-  } else {
-    let storedName = localStorage.getItem("name");
-    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
-  }
-myButton.onclick = function () {
-    setUserName();
-};  
+// let myButton = document.querySelector("button");
+// let myHeading = document.querySelector("h1");
+// function setUserName() {
+//     let myName = prompt("请输入你的名字。");
+//     if(!myName){
+//         setUserName();
+//     }else{
+//         localStorage.setItem("name", myName);
+//         myHeading.textContent = "Mozilla 酷毙了，" + myName;
+//     }
+// }
+// if (!localStorage.getItem("name")) {
+//     setUserName();
+//   } else {
+//     let storedName = localStorage.getItem("name");
+//     myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+//   }
+// myButton.onclick = function () {
+//     setUserName();
+// };  
